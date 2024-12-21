e541497bd4de4f1291a014012c54b3249a3bf97f
Dungchill <167697801+Dungchill@users.noreply.github.com>
2024-12-21 22:30:51 +0700

    Add files via upload

---
+++ web_csdl/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>web phim sách</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header">
+         </div>
+        <div class="menu">
+            <ul class="list_menu">
+                <li><a href="index.php">TRANG CHỦ</a></li>
+                <li><a href="index.php">TOP PHIM</a></li>
+                <li><a href="index.php">PHIM LẺ</a></li>
+                <li><a href="index.php">PHIM BỘ</a></li>
+                <li><a href="index.php">SÁCH</a></li>
+                <li><a href="index.php">TRAILER</a></li>
+            </ul>
+        </div>
+        <div class="main">
+            <ul class="movie_list">
+                <li>
+                    <img src="images/arcane.png">
+                </li>
+                <li>
+                    <img src="images/arcane.png">
+                </li>
+                <li>
+                    <img src="images/arcane.png">
+                </li>
+                <li>
+                    <img src="images/arcane.png">
+                </li>
+                <li>
+                    <img src="images/arcane.png">
+                </li>
+                <li>
+                    <img src="images/arcane.png">
+                </li>
+                <li>
+                    <img src="images/arcane.png">
+                </li>
+                <li>
+                    <img src="images/arcane.png">
+                </li>
+                <li>
+                    <img src="images/arcane.png">
+                </li>
+                <li>
+                    <img src="images/arcane.png">
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="footer">
+            <p>Copyright by Nguyễn Tiến Dũng 23021498</p>
+    </div>
+</body>
+</html>
